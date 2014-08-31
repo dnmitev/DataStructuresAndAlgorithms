88a102a10a55792e344ab00d5aa9306717efec3e
Dimitar Mitev <dnmitev@gmail.com>
2014-08-31 21:40:50 +0300

    PriorityQueue task is done

---
+++ AdvancedDataStructures/CustomPriorityQueueImplementation/Comparer.cs
@@ -0,0 +1,15 @@
+﻿
+namespace CustomPriorityQueueImplementation
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Comparer : IComparer<int>
+    {
+        public int Compare(int x, int y)
+        {
+            return x - y;
+        }
+    }
+}
---
+++ AdvancedDataStructures/CustomPriorityQueueImplementation/Program.cs
@@ -0,0 +1,30 @@
+﻿// 01. Implement a class PriorityQueue<T> based on the data structure "binary heap".
+
+namespace CustomPriorityQueueImplementation
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class Program
+    {
+        private static void Main()
+        {
+            var priorityQueue = new PriorityQueue<int, string>();
+            var rndGenerator = new Random();
+
+            for (int i = 0; i < 100; i++)
+            {
+                int number = rndGenerator.Next(0, 100);
+                var pair = new KeyValuePair<int, string>(number, string.Format("pesho{0}", number));
+
+                priorityQueue.Add(pair);
+            }
+
+            foreach (var item in priorityQueue)
+            {
+                Console.WriteLine("{0} : {1}", item.Key, item.Value);
+            }
+        }
+    }
+}
