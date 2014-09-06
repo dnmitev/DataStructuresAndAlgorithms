f8cc4887d917585cc0da7555300dc8f295db2c25
Dimitar Mitev <dnmitev@gmail.com>
2014-09-06 17:03:40 +0300

    subsets of given strings set task is done

---
+++ Recursion/AllSubsetsOfGivenStrings/Program.cs
@@ -0,0 +1,38 @@
+﻿// 06. Write a program for generating and printing all subsets of k strings from given set of strings.
+// Example: s = {test, rock, fun}, k=2 -> (test rock),  (test fun),  (rock fun)
+
+namespace AllSubsetsOfGivenStrings
+{
+    using System;
+    using System.Linq;
+
+    internal class Program
+    {
+        private static void Main()
+        {
+            int n = 3;
+            int k = 2;
+
+            string[] set = new string[3] { "beer", "whiskey", "rum" };
+            string[] output = new string[k];
+
+            FindCombinations(0, n, 0, set, output);
+        }
+
+        private static void FindCombinations(int index, int n, int start, string[] set, string[] output)
+        {
+            if (index == output.Length)
+            {
+                Console.WriteLine(string.Join(" ", output));
+            }
+            else
+            {
+                for (int i = start; i < set.Length; i++)
+                {
+                    output[index] = set[i];
+                    FindCombinations(index + 1, n, i + 1, set, output);
+                }
+            }
+        }
+    }
+}
